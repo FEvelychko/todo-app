d222778d85a90a13b452eb06a88c5223a076e8ab
FEvelychko <maxdevelopenko@gmail.com>
2015-09-30 13:51:20 +0300

    setting event handlering

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
-/**
- * Created by Maksym on 7/31/2015.
- */
 
-    (function () {
-        "use strict";
 
-        function Todo(){
-            this.editData = new EditData(this.addTemplate);
-            this.addTemplate = new AddTemplate();
-        };
-        var todo = new Todo();
+    var btn = document.getElementById('btn');
+    var input = document.getElementById('input');
+    var block = document.getElementById('block');
 
-    }());
+    function updateValue(){
+        return input.value = "";
+    }
+
+    function addNewToDo(){
+        var p = document.createElement('p');
+        p.innerHTML = "asasas";
+        block.appendChild(p);
+    }
+
+    function eventHandler(callUpdate){
+        var val = input.value;
+        console.log(val);
+        callUpdate();
+        addNewToDo();
+    }
+
+    btn.addEventListener('click', function(){
+        return eventHandler(updateValue);
+    }, false);
--- js/editData.js
+++ js/editData.js
@@ -1,33 +1,33 @@
-/**
- * Created by Maksym on 7/31/2015.
- */
-
-
-    (function (){
-        //"use strict";
-
-            function EditData(addTemplate){
-                var that = this;
-                that.addTemplate = addTemplate;
-                var template = document.createElement("div");
-                var element = document.getElementById("block");
-                if(event.keyCode == 13){
-                    template.setAttribute('class', 'newblock');
-                    template.innerHTML = '<p id = "textInput" class = "textInput"></p>';
-                    that.result = document.getElementById("newTodo").value;
-                    template.appendChild(document.createTextNode(that.result));
-                    document.getElementById("newTodo").value = "";
-                    return element.appendChild(template);
-                }
-            };
-
-            var handler  = document.addEventListener('keyup', EditData);
-        }());
-
-
-
-
-
-
-
-
+///**
+// * Created by Maksym on 7/31/2015.
+// */
+//
+//
+//    (function (){
+//        //"use strict";
+//
+//            function EditData(addTemplate){
+//                var that = this;
+//                that.addTemplate = addTemplate;
+//                var template = document.createElement("div");
+//                var element = document.getElementById("block");
+//                if(event.keyCode == 13){
+//                    template.setAttribute('class', 'newblock');
+//                    template.innerHTML = '<p id = "textInput" class = "textInput"></p>';
+//                    that.result = document.getElementById("newTodo").value;
+//                    template.appendChild(document.createTextNode(that.result));
+//                    document.getElementById("newTodo").value = "";
+//                    return element.appendChild(template);
+//                }
+//            };
+//
+//            var handler  = document.addEventListener('keyup', EditData);
+//        }());
+//
+//
+//
+//
+//
+//
+//
+//
