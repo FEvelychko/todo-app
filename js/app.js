167e66a1d0b2b5f6344871d6a1a844c02005c35e
FEvelychko <maxdevelopenko@gmail.com>
2015-09-30 17:31:33 +0300

    adding todo template and checkig the value

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 
-    var btn = document.getElementById('btn');
+    //"use strict";
+
+    //var btn = document.getElementById('btn');
     var input = document.getElementById('input');
     var block = document.getElementById('block');
 
@@ -8,19 +10,63 @@
         return input.value = "";
     }
 
-    function addNewToDo(){
-        var p = document.createElement('p');
-        p.innerHTML = "asasas";
-        block.appendChild(p);
+    function check(value, p, div){
+        if(value === ""){
+            return false;
+        }
+        else{
+            div.appendChild(p);
+            block.appendChild(div);
+        }
+    }
+
+    function addNewToDo(info){
+        var div = document.createElement('div');
+        var p = document.createElement('input');
+        div.className = "new-block-todo";
+        p.className = "new-todo";
+        p.value = info;
+        check(info, p, div);
     }
 
     function eventHandler(callUpdate){
         var val = input.value;
         console.log(val);
         callUpdate();
-        addNewToDo();
+        addNewToDo(val);
     }
 
-    btn.addEventListener('click', function(){
+    function inputKeyUp(e){
+        e.which = e.which || e.keyCode;
+        if(e.which == 13) {
+            return eventHandler(updateValue);
+        }
+        else{
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
+
+    /*btn.addEventListener('click', function(){
         return eventHandler(updateValue);
-    }, false);
+    }, false);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
