0c6cc658364fca14edac07f1239dfd8ba49746d6
FEvelychko <maxdevelopenko@gmail.com>
2015-10-01 20:09:15 +0300

    showing left items and deleting the values

--- js/app.js
+++ js/app.js
@@ -4,33 +4,66 @@
 
     var inp = document.getElementById('input');
     var todoList = document.getElementById('todo-list');
+    var leftItems = document.getElementById('left-items');
+    var menuDiv = document.querySelector('div.menu');
+    var childrenToDo = 0;
 
     function updateValue(){
+
         return inp.value = "";
+
     }
 
-    function check(value, li, input, btn){
+    function check(value, li, checkbox, input, btn){
+
         if(value !== ""){
+            li.appendChild(checkbox);
             li.appendChild(input);
             li.appendChild(btn);
             todoList.appendChild(li);
         }
+
     }
 
     function deleteValue(){
-        //debugger;
-        this.style.backgroundColor = "red";
+
         todoList.removeChild(this.parentElement);
+        showInclDelToDo();
         //for(var x = 0; x < 2; x++){
             //this.parentElement.removeChild(this.parentElement.childNodes[0]);
         //}
+
+    }
+
+    function showInclDelToDo(){
+
+        childrenToDo = todoList.children.length;
+        leftItems.innerHTML = Math.floor(childrenToDo) + " items left";
+        if(childrenToDo === 0){
+            menuDiv.style.display = "none";
+        }
+        return leftItems;
+
+    }
+
+    function showTotalToDo(){
+
+        childrenToDo = todoList.children.length;
+        leftItems.innerHTML = Math.floor(childrenToDo + 1) + " items left";
+        return leftItems;
+
     }
 
     function addNewToDo(info){
+
         var li = document.createElement('li');
+        var checkbox = document.createElement('input');
         var input = document.createElement('input');
         var btn = document.createElement('button');
 
+        menuDiv.style.display = "block";
+        checkbox.type = "checkbox";
+        checkbox.className = "checkbox";
         li.className = "new-li";
         input.className = "add-new-todo";
         btn.className = "btn-deleted";
@@ -38,17 +71,21 @@
         btn.innerHTML = "delete";
         input.value = info;
         btn.onclick = deleteValue;
-        check(info, li, input, btn);
+        showTotalToDo();
+        check(info, li, checkbox, input, btn);
 
     }
 
     function eventHandler(callUpdate){
+
         var val = inp.value;
         callUpdate();
         addNewToDo(val);
+
     }
 
     function inputKeyUp(e){
+
         e.which = e.which || e.keyCode;
         if(e.which == 13) {
             return eventHandler(updateValue);
@@ -56,7 +93,10 @@
         else{
             return false;
         }
+
     }
+
+
 
 
 
@@ -81,16 +121,3 @@
     /*btn.addEventListener('click', function(){
         return eventHandler(updateValue);
     }, false);*/
-
-
-
-
-
-
-
-
-
-
-
-
-
